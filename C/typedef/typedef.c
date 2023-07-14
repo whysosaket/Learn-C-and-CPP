09e941a2d0b6c504bdb7ea28ba327ba9b2c61615
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-15 04:51:35 +0530

    typedef

---
+++ C/typedef/typedef.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+typedef char user[25];
+
+int main(){
+    // typedef = reserved keyword that gives an existing datatype a name
+    // this is very tedious and to avoid this writing again and again we can use the typedef feature
+    char user1[25] = "Bro";
+    user user2 = "Bro2";
+    return 0;
+}
