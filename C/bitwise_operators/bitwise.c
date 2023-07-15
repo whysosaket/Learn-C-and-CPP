7dcdc9878062fcf15b90868205232e9b2096bb2a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-07-16 02:13:32 +0530

    bitwise operator

---
+++ C/bitwise_operators/bitwise.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+    // BITWISE OPERATORS in C
+
+    // & -> AND
+    // | -> OR
+    // ^ -> XOR
+    // << -> Leftshift
+    // >> -> Rightshift
+
+    int x = 6;      // 00000110
+    int y = 12;     // 00001100
+
+    // int z=x&y;
+    // int z=x|y;
+    // int z=x^y;
+    // int z=x<<1;
+    // int z=x>>1;
+
+    printf("%d\n",z);
+
+    return 0;
+}
